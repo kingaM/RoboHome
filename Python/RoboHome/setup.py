d38ba4c4af59d796476b20e2b7a3246001845dc5
michboon <michboon@email>
2013-02-12 17:57:33 +0000

    Deleted old files

--- Python/RoboHome/setup.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-from setuptools import setup, find_packages
-
-setup(version='0.1',
-      description='Home Automation system of awesome',
-      author='COMP2014 Group 7',
-      packages=find_packages(),
-      test_suite="tests",
-)
