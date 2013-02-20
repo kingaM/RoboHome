07f2e0f8eaf734aab086d24cf26c1fd7bc4e5d7f
kingaM <kingaM@example.com>
2013-02-20 11:45:45 +0100

    Added auto redirect on login, no cookies

--- web/RoboHome/robohome/static/js/openid-jquery.js
+++ web/RoboHome/robohome/static/js/openid-jquery.js
@@ -40,7 +40,6 @@ openid = {
 	 */
 	init : function(input_id, return_url_id, return_url_name, return_url) {
 		providers = $.extend({}, providers_large);
-		var openid_btns = $('#openid_btns');
 		this.input_id = input_id;
 		this.return_url_id = return_url_id;
 		this.return_url = return_url;
@@ -48,10 +47,6 @@ openid = {
 		$('#openid_choice').show();
 		$('#openid_input_area').empty();
 		$('#openid_form').submit(this.submit);
-		// var box_id = this.readCookie();
-		// if (box_id) {
-		// 	this.signin(box_id, true);
-		// }
 		this.signin('google', false)
 	},
 
@@ -65,7 +60,6 @@ openid = {
 		if (!provider) {
 			return;
 		}
-		this.setCookie(box_id);
 		this.provider_id = box_id;
 		this.provider_url = provider.url;
 		// prompt user for input?
@@ -124,30 +118,5 @@ openid = {
 			$('#openid_form').append('<input type="hidden" id="' + this.return_url_id + '" name="' + this.return_url_name + '" value="' + this.return_url + '"/>');
 		}		
 	},
-
-
-	setCookie : function(value) {
-		var date = new Date();
-		date.setTime(date.getTime() + (this.cookie_expires * 24 * 60 * 60 * 1000));
-		var expires = "; expires=" + date.toGMTString();
-		document.cookie = this.cookie_name + "=" + value + expires + "; path=" + this.cookie_path;
-	},
-
-	readCookie : function() {
-		var nameEQ = this.cookie_name + "=";
-		var ca = document.cookie.split(';');
-		for ( var i = 0; i < ca.length; i++) {
-			var c = ca[i];
-			while (c.charAt(0) == ' ')
-				c = c.substring(1, c.length);
-			if (c.indexOf(nameEQ) == 0)
-				return c.substring(nameEQ.length, c.length);
-		}
-		return null;
-	},
-
-	setDemoMode : function(demoMode) {
-		this.demo = demoMode;
-	}
 };
 })(jQuery);
